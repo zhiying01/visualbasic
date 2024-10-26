0b5e703164957c1d88b0f132c49bdf10ba3fb0ea
zys <148034118+zhiying01@users.noreply.github.com>
2024-10-26 23:05:26 +0800

    Add files via upload

---
+++ Form1/MainWindow.xaml.vb
@@ -0,0 +1,24 @@
+﻿Class MainWindow
+    Private Sub Button1_Click(sender As Object, e As EventArgs)
+        'find smaller uber
+        Dim a, b, smallest As Integer
+        a = Val(txt1.Text)
+        b = Val(txt2.Text)
+        If a < b Then
+            a = smallest
+        ElseIf b < a Then
+            b = smallest
+        End If
+
+        'find the GCD
+        For c As Integer = 1 To smallest
+
+            If a Mod c = 0 And b Mod c = 0 Then
+                ListBox1.Items.Add(c)
+
+            End If
+        Next
+        ' ListBox1.Items.Add(c)
+    End Sub
+
+End Class
